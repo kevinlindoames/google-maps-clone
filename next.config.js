f66e36bf60305d7d43e77d4285ee7188c7b57780
Kevin Lindo Ames <82423639+kevinlindoames@users.noreply.github.com>
2023-04-08 14:39:44 -0500

    5.5

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  output: 'export',
-  distDir: 'build',
+  reactStrictMode: false, 
 }
 
 module.exports = nextConfig
